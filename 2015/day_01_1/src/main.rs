bb23c94678f55e50c9eeb292341cc71c2bcb61ae
Maximilian Köstler <maximilian@koestler.hamburg>
2024-04-08 06:42:50 +0200

    Add more docs to 2015 day 1 part 1

--- 2015/day_01_1/src/main.rs
+++ 2015/day_01_1/src/main.rs
@@ -1,5 +1,12 @@
+//! Advent of code 2015 day 1 part 2
+//! Restrictions for today:
+//!   - As many iterator adaptors as possible
+//!   - No manual loops
+//!   - No non-std dependencies
+
 use std::io::Read;
 
+/// Gets the final floor after following all instructions.
 fn get_floor(chars: impl Iterator<Item = char>) -> i32 {
     chars.fold(0, |floor, c| match c {
         '(' => floor + 1,
@@ -26,6 +33,7 @@ mod tests {
 
     #[test]
     fn test_get_floor() {
+        assert_eq!(get_floor("".chars()), 0);
         assert_eq!(get_floor("(())".chars()), 0);
         assert_eq!(get_floor("()()".chars()), 0);
         assert_eq!(get_floor("(((".chars()), 3);
