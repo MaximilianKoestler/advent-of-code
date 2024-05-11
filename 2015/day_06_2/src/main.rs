9569c8e426f2acadcdd562f2c3b3cdf14c20a634
Maximilian Köstler <maximilian@koestler.dev>
2024-05-11 09:59:54 +0200

    Implement 2015/06-2

--- 2015/day_06_2/src/main.rs
+++ 2015/day_06_2/src/main.rs
@@ -1,4 +1,4 @@
-//! Advent of code 2015 day 6 part 1
+//! Advent of code 2015 day 6 part 2
 //! Restrictions for today:
 //!   - As many iterator adaptors as possible
 //!   - No manual loops
@@ -8,52 +8,52 @@ use std::io::BufRead;
 
 const GRID_SIZE: usize = 1000;
 struct LightGrid {
-    lights: Vec<Vec<bool>>,
+    lights: Vec<Vec<usize>>,
 }
 
 impl LightGrid {
     fn new() -> Self {
         Self {
-            lights: vec![vec![false; GRID_SIZE]; GRID_SIZE],
+            lights: vec![vec![0; GRID_SIZE]; GRID_SIZE],
         }
     }
 
     fn apply_instruction(&mut self, instruction: &Instruction) {
         (instruction.start.x..=instruction.end.x).for_each(|x| {
             (instruction.start.y..=instruction.end.y).for_each(|y| match instruction.action {
-                Action::TurnOn => self.lights[x][y] = true,
-                Action::TurnOff => self.lights[x][y] = false,
-                Action::Toggle => self.lights[x][y] = !self.lights[x][y],
+                Action::TurnOn => self.lights[x][y] += 1,
+                Action::TurnOff => self.lights[x][y] = self.lights[x][y].saturating_sub(1),
+                Action::Toggle => self.lights[x][y] += 2,
             });
         });
     }
 
-    fn count_lit_lights(&self) -> usize {
+    fn sum_brightness(&self) -> usize {
         self.lights
             .iter()
-            .map(|row| row.iter().map(|&light| light as usize).sum::<usize>())
-            .sum()
+            .map(|row| row.iter().sum::<usize>())
+            .sum::<usize>()
     }
 }
 
-fn count_lit_lights(instructions: impl Iterator<Item = Instruction>) -> usize {
+fn sum_brightness(instructions: impl Iterator<Item = Instruction>) -> usize {
     let mut grid = LightGrid::new();
     instructions.for_each(|instruction| grid.apply_instruction(&instruction));
-    grid.count_lit_lights()
+    grid.sum_brightness()
 }
 
 fn main() {
-    let file = std::fs::File::open("input/input.txt").unwrap();
+    let file = std::fs::File::open("../day_06_1/input/input.txt").unwrap();
     let reader = std::io::BufReader::new(file);
 
-    let lit_lights = count_lit_lights(
+    let lit_lights = sum_brightness(
         reader
             .lines()
             .map(Result::unwrap)
             .map(|line| Instruction::try_from(line.as_ref()).unwrap()),
     );
 
-    println!("Lit lights: {}", lit_lights);
+    println!("Total brightness: {}", lit_lights);
 }
 
 #[cfg(test)]
@@ -62,14 +62,14 @@ mod tests {
     use day_06_1::Coordinate;
 
     #[test]
-    fn test_count_lit_lights() {
+    fn test_sum_brightness() {
         let mut instructions = vec![Instruction {
             action: Action::TurnOn,
             start: Coordinate { x: 0, y: 0 },
             end: Coordinate { x: 999, y: 999 },
         }];
         assert_eq!(
-            count_lit_lights(instructions.clone().into_iter()),
+            sum_brightness(instructions.clone().into_iter()),
             1_000 * 1_000
         );
 
@@ -79,8 +79,8 @@ mod tests {
             end: Coordinate { x: 999, y: 0 },
         }]);
         assert_eq!(
-            count_lit_lights(instructions.clone().into_iter()),
-            (1_000 - 1) * 1_000
+            sum_brightness(instructions.clone().into_iter()),
+            1_000 * 1_000 + 2_000
         );
 
         instructions.extend(vec![Instruction {
@@ -89,8 +89,8 @@ mod tests {
             end: Coordinate { x: 500, y: 500 },
         }]);
         assert_eq!(
-            count_lit_lights(instructions.clone().into_iter()),
-            (1_000 - 1) * 1_000 - 4
+            sum_brightness(instructions.clone().into_iter()),
+            1_000 * 1_000 + 2_000 - 4
         );
     }
 }
