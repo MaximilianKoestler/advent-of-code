f7c6a1008d6be97d47b8bd83c540c8376d361173
Maximilian Köstler <maximilian@koestler.hamburg>
2024-05-24 19:35:03 +0200

    Make nom parsing more idiomatic

--- 2015/day_07_1/src/wire.rs
+++ 2015/day_07_1/src/wire.rs
@@ -35,7 +35,7 @@ mod parsers {
         bytes::complete::tag,
         character::complete::alpha1,
         combinator::{map, map_res},
-        sequence::{pair, separated_pair},
+        sequence::{preceded, separated_pair},
         IResult,
     };
 
@@ -54,27 +54,34 @@ mod parsers {
     }
 
     fn gate_not(input: &str) -> IResult<&str, Gate> {
-        pair(tag("NOT "), signal)(input).map(|(input, (_, signal))| (input, Gate::Not(signal)))
+        map(preceded(tag("NOT "), signal), Gate::Not)(input)
     }
 
     fn gate_and(input: &str) -> IResult<&str, Gate> {
-        separated_pair(signal, tag(" AND "), signal)(input)
-            .map(|(input, (lhs, rhs))| (input, Gate::And(lhs, rhs)))
+        map(
+            separated_pair(signal, tag(" AND "), signal),
+            |(lhs, rhs)| Gate::And(lhs, rhs),
+        )(input)
     }
 
     fn gate_or(input: &str) -> IResult<&str, Gate> {
-        separated_pair(signal, tag(" OR "), signal)(input)
-            .map(|(input, (lhs, rhs))| (input, Gate::Or(lhs, rhs)))
+        map(separated_pair(signal, tag(" OR "), signal), |(lhs, rhs)| {
+            Gate::Or(lhs, rhs)
+        })(input)
     }
 
     fn gate_lshift(input: &str) -> IResult<&str, Gate> {
-        separated_pair(signal, tag(" LSHIFT "), value)(input)
-            .map(|(input, (lhs, rhs))| (input, Gate::LShift(lhs, rhs)))
+        map(
+            separated_pair(signal, tag(" LSHIFT "), value),
+            |(lhs, rhs)| Gate::LShift(lhs, rhs),
+        )(input)
     }
 
     fn gate_rshift(input: &str) -> IResult<&str, Gate> {
-        separated_pair(signal, tag(" RSHIFT "), value)(input)
-            .map(|(input, (lhs, rhs))| (input, Gate::RShift(lhs, rhs)))
+        map(
+            separated_pair(signal, tag(" RSHIFT "), value),
+            |(lhs, rhs)| Gate::RShift(lhs, rhs),
+        )(input)
     }
 
     pub fn gate(input: &str) -> IResult<&str, Gate> {
@@ -91,8 +98,10 @@ mod parsers {
     }
 
     pub fn wire(input: &str) -> IResult<&str, Wire> {
-        separated_pair(source, tag(" -> "), name)(input)
-            .map(|(input, (source, name))| (input, Wire { source, name }))
+        map(
+            separated_pair(source, tag(" -> "), name),
+            |(source, name)| Wire { source, name },
+        )(input)
     }
 }
 
@@ -195,141 +204,129 @@ mod tests {
 
     #[test]
     fn test_value() {
-        let wire = Wire::try_from("123 -> x").unwrap();
         assert_eq!(
-            wire,
-            Wire {
+            Wire::try_from("123 -> x"),
+            Ok(Wire {
                 source: Source::Value(123),
                 name: Name("x".to_string()),
-            }
+            })
         );
     }
 
     #[test]
     fn test_direct() {
-        let wire = Wire::try_from("y -> x").unwrap();
         assert_eq!(
-            wire,
-            Wire {
+            Wire::try_from("y -> x"),
+            Ok(Wire {
                 source: Source::Direct(Name("y".to_string())),
                 name: Name("x".to_string()),
-            }
+            })
         );
     }
 
     #[test]
     fn test_gate_not() {
-        let wire = Wire::try_from("NOT x -> z").unwrap();
         assert_eq!(
-            wire,
-            Wire {
+            Wire::try_from("NOT x -> z"),
+            Ok(Wire {
                 source: Source::Gate(Gate::Not(Signal::Connection(Name("x".to_string())))),
                 name: Name("z".to_string()),
-            }
+            })
         );
 
-        let wire = Wire::try_from("NOT 123 -> z").unwrap();
         assert_eq!(
-            wire,
-            Wire {
+            Wire::try_from("NOT 123 -> z"),
+            Ok(Wire {
                 source: Source::Gate(Gate::Not(Signal::Immediate(123))),
                 name: Name("z".to_string()),
-            }
+            })
         );
     }
 
     #[test]
     fn test_gate_and() {
-        let wire = Wire::try_from("x AND y -> z").unwrap();
         assert_eq!(
-            wire,
-            Wire {
+            Wire::try_from("x AND y -> z"),
+            Ok(Wire {
                 source: Source::Gate(Gate::And(
                     Signal::Connection(Name("x".to_string())),
                     Signal::Connection(Name("y".to_string()))
                 )),
                 name: Name("z".to_string()),
-            }
+            })
         );
 
-        let wire = Wire::try_from("xy AND 123 -> z").unwrap();
         assert_eq!(
-            wire,
-            Wire {
+            Wire::try_from("xy AND 123 -> z"),
+            Ok(Wire {
                 source: Source::Gate(Gate::And(
                     Signal::Connection(Name("xy".to_string())),
                     Signal::Immediate(123)
                 )),
                 name: Name("z".to_string()),
-            }
+            })
         );
     }
     #[test]
     fn test_gate_or() {
-        let wire = Wire::try_from("x OR y -> z").unwrap();
         assert_eq!(
-            wire,
-            Wire {
+            Wire::try_from("x OR y -> z"),
+            Ok(Wire {
                 source: Source::Gate(Gate::Or(
                     Signal::Connection(Name("x".to_string())),
                     Signal::Connection(Name("y".to_string()))
                 )),
                 name: Name("z".to_string()),
-            }
+            })
         );
 
-        let wire = Wire::try_from("123 OR x -> z").unwrap();
         assert_eq!(
-            wire,
-            Wire {
+            Wire::try_from("123 OR x -> z"),
+            Ok(Wire {
                 source: Source::Gate(Gate::Or(
                     Signal::Immediate(123),
                     Signal::Connection(Name("x".to_string())),
                 )),
                 name: Name("z".to_string()),
-            }
+            })
         );
     }
 
     #[test]
     fn test_gate_lshift() {
-        let wire = Wire::try_from("x LSHIFT 2 -> z").unwrap();
         assert_eq!(
-            wire,
-            Wire {
+            Wire::try_from("x LSHIFT 2 -> z"),
+            Ok(Wire {
                 source: Source::Gate(Gate::LShift(Signal::Connection(Name("x".to_string())), 2)),
                 name: Name("z".to_string()),
-            }
+            })
         );
 
-        let wire = Wire::try_from("123 LSHIFT 2 -> z").unwrap();
         assert_eq!(
-            wire,
-            Wire {
+            Wire::try_from("123 LSHIFT 2 -> z"),
+            Ok(Wire {
                 source: Source::Gate(Gate::LShift(Signal::Immediate(123), 2)),
                 name: Name("z".to_string()),
-            }
+            })
         );
     }
 
     #[test]
     fn test_gate_rshift() {
-        let wire = Wire::try_from("x RSHIFT 2 -> z").unwrap();
         assert_eq!(
-            wire,
-            Wire {
+            Wire::try_from("x RSHIFT 2 -> z"),
+            Ok(Wire {
                 source: Source::Gate(Gate::RShift(Signal::Connection(Name("x".to_string())), 2)),
                 name: Name("z".to_string()),
-            }
+            })
         );
 
-        let wire = Wire::try_from("123 RSHIFT 2 -> z").unwrap();
         assert_eq!(
-            wire,
-            Wire {
+            Wire::try_from("123 RSHIFT 2 -> z"),
+            Ok(Wire {
                 source: Source::Gate(Gate::RShift(Signal::Immediate(123), 2)),
                 name: Name("z".to_string()),
-            }
+            })
         );
     }
 }
