9de47a6b9e09125f6fdf7eff223655e0bcb51cbf
Maximilian Köstler <maximilian@koestler.dev>
2024-06-16 09:20:57 +0200

    Implement 2015/11-1

---
+++ 2015/day_11_1/src/lib.rs
@@ -0,0 +1,24 @@
+/// Represents the result of a password check.
+#[derive(Debug)]
+pub enum CheckResult {
+    /// The password check is still undecided.
+    Undecided,
+    /// The string is definitely a valid password.
+    Valid,
+    /// The string is definitely invalid (includes the requirement numbers).
+    Invalid(u32),
+}
+
+/// Represents a token in the input string.
+#[derive(Debug)]
+pub enum Token {
+    /// A character token.
+    Char(u8),
+    /// The end of the string token.
+    End,
+}
+
+pub trait PasswordChecker {
+    /// Consumes a token and returns the result of the password check.
+    fn consume(&mut self, position: usize, token: Token) -> CheckResult;
+}
